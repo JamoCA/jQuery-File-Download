cd5aa3cf4ffbd1542d7b298d76cf7c47661ee6c4
James Moberg <james@sunstarmedia.com>
2022-08-30 15:22:12 -0700

    Post-fork updates w/target & textarea
    Update to support `target` parameter. Textarea used in case values contain commas.

--- jQuery.download.js
+++ jQuery.download.js
@@ -6,22 +6,24 @@
  * reference article: http://www.filamentgroup.com/lab/jquery_plugin_for_requesting_ajax_like_file_downloads/
  * Copyright (c) 2008 Filament Group, Inc
  * Dual licensed under the MIT (filamentgroup.com/examples/mit-license.txt) and GPL (filamentgroup.com/examples/gpl-license.txt) licenses.
+ * Updated 2022-08-30 by James Moberg - SunStar Media https://www.sunstarmedia.com/
  * --------------------------------------------------------------------
  */
  
-jQuery.download = function(url, data, method){
+jQuery.download = function(url, data, method, target){
+	if (typeof target === 'undefined') { target = ''; }
 	//url and data options required
-	if( url && data ){ 
+	if( url && data ){
 		//data can be string of parameters or array/object
 		data = typeof data == 'string' ? data : jQuery.param(data);
 		//split params into form inputs
 		var inputs = '';
-		jQuery.each(data.split('&'), function(){ 
+		jQuery.each(data.split('&'), function(){
 			var pair = this.split('=');
-			inputs+='<input type="hidden" name="'+ pair[0] +'" value="'+ pair[1] +'" />'; 
+			inputs+='<textarea type="hidden" name="'+ pair[0] +'">'+ pair[1] +'</textarea>';
 		});
 		//send request
-		jQuery('<form action="'+ url +'" method="'+ (method||'post') +'">'+inputs+'</form>')
+		jQuery('<form action="'+ url +'" method="'+ (method||'post') +'" target="'+ target +'">'+inputs+'</form>')
 		.appendTo('body').submit().remove();
 	};
 };
